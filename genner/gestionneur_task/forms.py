c53aa9448e51bbde8b799aa8fc239598abe78cc7
aleckklt <aleckkouletio@gmail.com>
2025-09-08 15:31:03 +0100

    gestionnaire added and notifications gestion + exchanges_values

--- genner/gestionneur_task/forms.py
+++ genner/gestionneur_task/forms.py
@@ -5,11 +5,3 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Tasks
         fields = ['titre', 'statut', 'date', 'heure', 'commentaire']
-
-        def clean(self):
-            cleaned_data = super().clean()
-            statut = cleaned_data.get('statut')
-            commentaire = cleaned_data.get('commentaire')
-
-            if statut == 'Terminé' and not commentaire :
-                raise forms.ValidationError('Veuillez ajouter un commentaire lorsque la tâche est terminée.')
